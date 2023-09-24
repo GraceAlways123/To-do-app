d470f728db8c11a17455a8fc393e1edb1fcbd55e
Chukwunonso Okafor <Chukwunonso.Okafor@mpinfrastructure.local>
2023-09-24 21:28:17 +0100

    modified: frontEnd-GUI.py

--- frontEnd_GUI.py
+++ frontEnd_GUI.py
@@ -4,7 +4,7 @@ import PySimpleGUI as PSG
 inputLabel = PSG.Text('List of to do')
 inputText = PSG.InputText(tooltip='Enter your to do phrase')
 inputButton = PSG.Button('Add')
-window = PSG.Window('My to do App', layout=[[inputLabel], [inputText, inputButton]])
+window = PSG.Window('My to do App', layout=[[inputLabel], [inputText, inputButton]], font=('Helvetica', 20))
 
 window.read()
 window.close()
